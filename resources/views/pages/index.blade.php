5a94c2a492dbca34355812bfe9ea36b3536c2015
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-17 20:13:52 +0100

    style: cards in index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,17 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello</h1>
+    <h1>Comics: {{count($comics)}}</h1>
+        <ul class="row d-flex justify-content-center">
+            @foreach ($comics as $comic)
+
+            <li class="card col-3 p-1 m-2">
+                <h1 style="color: purple">{{$comic -> title}}</h1>
+                <h2>{{$comic -> author}}</h2>
+                <h3>{{$comic -> price}}$</h3>
+            </li>
+                
+            @endforeach
+        </ul>
+
 @endsection
